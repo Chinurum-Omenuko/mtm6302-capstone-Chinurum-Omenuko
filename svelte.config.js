7ff4f684c731e1cd79ec2f663db3cfe952536299
Chinurum-Omenuko <122641413+Chinurum-Omenuko@users.noreply.github.com>
2024-03-12 10:56:57 -0400

    completes assignment

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
             fallback: '200.html'
         }),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/mtm6302-capstone-Chinurum-Omenuko' : '',
+            base: '/mtm6302-capstone-Chinurum-Omenuko',
         }
         // other kit options...
     },
